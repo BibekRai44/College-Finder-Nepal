db2a53393821943c11eded59df264541e1d6bb6b
Bibekkun <bibekrai655@gmail.com>
2023-04-05 23:10:07 +0545

    if else condition

--- info.py
+++ info.py
@@ -9,12 +9,18 @@ with open('hrefs.txt','r') as file:
 for url in urls:
         response = requests.get(url)
         soup = BeautifulSoup(response.content, 'html.parser')
-
-        University = soup.find('span', class_="inline-flex justify-center h-12 w-12 text-lg").text
+    
+        college=soup.find('div',class_='org-title col-span-8').find('h1',class_="title-font text-xl md:text-4xl pb-1 md:pb-4 font-bold leading-5 py-4 z-20").text.strip()
+        #if college:
+              #college_text=college.text.strip().replace('✓','')
+        #else:
+             # college_text=''
+        #University = soup.find('ul',class_="flex flex-col bg-gray-100 pt-8 rounded-r-2xl text-sm text-gray-500").find('li', title='Accreditation').text
         data={
-              'University':University
+               'College':college.replace('✓','')
+                #'University':University
         }
         datalist.append(data)
-
+        
 df=pd.DataFrame(datalist)
 df.to_csv('college.csv',index=True)
