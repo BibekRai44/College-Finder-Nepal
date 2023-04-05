443acf5f91444e1a36907af6c863602cab0c26b4
Bibekkun <bibekrai655@gmail.com>
2023-04-05 21:32:12 +0545

    scraped college data

--- scraper.py
+++ scraper.py
@@ -1,16 +1,17 @@
 import requests
 from bs4 import BeautifulSoup
-urls=['https://www.linkedin.com/jobs/search/?currentJobId=3531688286&distance=25&geoId=104630404&keywords=jobs&location=Nepal&refresh=true']
+
+urls = ['https://edusanjal.com/college/?page=1']
 
 with open('hrefs.txt', 'w') as file:
-
     for url in urls:
         response = requests.get(url)
         soup = BeautifulSoup(response.content, 'html.parser')
-        
-        parent_links = soup.find('a', class_="disabled ember-view job-card-container__link job-card-list__title")
+
+        parent_links = soup.find_all('div', class_="overflow-hidden bg-white rounded shadow-xl")
         if parent_links:
-            href_value=parent_links.get('href')
-            file.write(href_value + '\n')
+            for link in parent_links:
+                href_value = link.find('a').get('href')
+                file.write("https://edusanjal.com"+href_value + '\n')
         else:
             print("None")
